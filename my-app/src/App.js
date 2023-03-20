205b658d24dbe8e84a941f1136368315617981f6
hacles100 <haclesrocha200@gmail.com>
2023-03-20 22:21:17 -0100

    conceito componente done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Header from './components/Header';
 
 function App() {
   return (
-    <div>
-      <h1>Ola Mundo!</h1>
-    </div>
+    <>
+      <Header/>
+      <Header/>
+    </>
   );
 }
 
