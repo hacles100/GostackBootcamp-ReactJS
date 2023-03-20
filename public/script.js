1463b0725cc611e2e20be0bb279f97faa25b413c
hacles100 <haclesrocha200@gmail.com>
2023-03-20 21:07:56 -0100

    webpack config

---
+++ public/script.js
@@ -0,0 +1,4 @@
+/** 
+ * Babel: converter codigo do React para um codigo que o browser entende
+ * Webpack: Para cada tipo de arquivo (.js, .css, .png) eu vou converter o codigo de forma diferente
+*/
---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    entry: Path.resolve(__dirname, 'src', 'index.js'),
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            }
+        ]
+    },
+};
