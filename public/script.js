c9c1a61f78461847cf16a4995c5d1cf3feb1c5ef
hacles100 <haclesrocha200@gmail.com>
2023-03-20 21:57:24 -0100

    changes

--- public/script.js
+++
@@ -1,4 +0,0 @@
-/** 
- * Babel: converter codigo do React para um codigo que o browser entende
- * Webpack: Para cada tipo de arquivo (.js, .css, .png) eu vou converter o codigo de forma diferente
-*/
