f80a5b5f5aa304eb623deea5a1119b0807f675d5
hacles100 <haclesrocha200@gmail.com>
2023-03-21 21:50:33 -0100

    state done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Header from './components/Header';
 
 function App() {
+
+  const [projects, setProjects] = useState(['Development', 'Web App', 'Mobile App']);
+
+  function handleAddProject () {
+  
+    setProjects([...projects, `Novo projeto ${Date.now()}`]);
+
+    console.log(projects)
+  }
+
   return (
     <>
-      <Header title="HomePage"/>
       <Header title="Projects"/>
+
+      <ul>
+        {projects.map(project => <li key={project}>{project}</li>)}
+      </ul>
+
+      <button type='button' onClick={handleAddProject}>Adicionar Projecto</button>
     </>
   );
 }
